0504c51f5a13b8ae268cf0e767353d2acc672938
nicolasarvani <nicolas.arvani@gmail.com>
2023-12-05 23:08:39 -0300

    Refactor CatImage class to include a loading indicator for the network image

--- lib/src/models/cat_image.dart
+++ lib/src/models/cat_image.dart
@@ -1,25 +1,39 @@
-
-
 import 'package:flutter/material.dart';
 
-class CatImage{
+class CatImage {
+  String id = '';
+  String url = '';
+  int width = 0;
+  int height = 0;
+  Image imagem = Image.network('');
 
-	String id = '';
-	String url = '';
-	int width = 0;
-	int height = 0;
-	Image imagem = Image.network('');
+  CatImage(this.id, this.url, this.width, this.height);
 
-	CatImage(this.id, this.url, this.width, this.height);
-
-	CatImage.fromJson(List<dynamic> json){
-		id = json[0]['breed'].toString();
-		url = json[0]['url'];
-		width = json[0]['width'];
-		height = json[0]['height'];
-		imagem = Image.network(url);
-	}
-
-
-
+  CatImage.fromJson(List<dynamic> json) {
+    id = json[0]['breed'].toString();
+    url = json[0]['url'];
+    width = json[0]['width'];
+    height = json[0]['height'];
+    imagem = Image.network(
+      url,
+      loadingBuilder: (context, child, loadingProgress) {
+        return loadingProgress == null
+            ? child
+            : SizedBox(
+                height: 200,
+                width: 200,
+                child: Center(
+                  child: Transform.scale(
+                    scale: 1.75,
+                    child: const CircularProgressIndicator(
+                      backgroundColor: Colors.white,
+                      color: Colors.greenAccent,
+                      strokeWidth: 6.66,
+                    ),
+                  ),
+                ),
+              );
+      },
+    );
+  }
 }
