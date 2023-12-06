f3548cbaa33d5348f99b1ef309576a1829e82bec
nicolasarvani <nicolas.arvani@gmail.com>
2023-12-05 23:56:28 -0300

    Update dotenv configuration

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async{
 
-  await dotenv.load();
+  await dotenv.load(fileName: 'assets/.env');
 
   var app = App();
   runApp(app);
